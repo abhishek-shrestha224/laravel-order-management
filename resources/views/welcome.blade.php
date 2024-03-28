2fd3f3cfb1dc3cb07583866b9bc28b6bfb903378
abhishek-shrestha224 <meabhi224@gmail.com>
2024-03-28 22:59:28 +0545

    added layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline font-heading">
-    Hello world!
-  </h1>
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('content')
+    <h1>Hello World</h1>
+@endsection
