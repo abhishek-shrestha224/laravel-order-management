2965aced8586be0586c3e790d4310b9847126374
abhishek-shrestha224 <meabhi224@gmail.com>
2024-03-29 15:59:46 +0545

    font changed to product.index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <section class="w-screen py-20 px-10">
-        <h1 class="text-6xl font-bold font-heading mb-12">Our Products</h1>
+        <h1 class="text-6xl font-bold font-heading mb-4">Our Products</h1>
         <div>
             @foreach ($products as $product)
-                <div>
-                    <h2>{{ $product['name'] }}</h2>-<p>{{ $product['weight'] }}</p>
+                <div class="flex text-3xl font-bold">
+                    <p class="font-heading">{{ $product['name'] }}</p>-<p class="font-heading">{{ $product['weight'] }}</p>
                 </div>
             @endforeach
         </div>
