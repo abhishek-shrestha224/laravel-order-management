5ddd1cbd5bb2493ebf267273a6255262f9ea2d44
abhishek-shrestha224 <meabhi224@gmail.com>
2024-03-30 00:12:12 +0545

    styled the register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,10 @@
                             @csrf
 
                             <div class="row mb-3">
-                                <label class="col-md-4 col-form-label text-md-end" for="name">{{ __('Name') }}</label>
+                                <label class="mb-2 block text-lg font-bold text-gray-900 dark:text-white" for="name">{{ __('Name') }}</label>
 
                                 <div class="col-md-6">
-                                    <input class="form-control @error('name') is-invalid @enderror" id="name"
+                                    <input class="form-control @error('name') is-invalid @enderror block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" id="name"
                                         type="text" name="name" value="{{ old('name') }}" required
                                         autocomplete="name" autofocus>
 
@@ -28,11 +28,11 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label class="col-md-4 col-form-label text-md-end"
+                                <label class="mb-2 block text-lg font-bold text-gray-900 dark:text-white"
                                     for="email">{{ __('Email Address') }}</label>
 
                                 <div class="col-md-6">
-                                    <input class="form-control @error('email') is-invalid @enderror" id="email"
+                                    <input class="form-control @error('email') is-invalid @enderror block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" id="email"
                                         type="email" name="email" value="{{ old('email') }}" required
                                         autocomplete="email">
 
@@ -45,11 +45,11 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label class="col-md-4 col-form-label text-md-end"
+                                <label class="mb-2 block text-lg font-bold text-gray-900 dark:text-white"
                                     for="password">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input class="form-control @error('password') is-invalid @enderror" id="password"
+                                    <input class="form-control @error('password') is-invalid @enderror block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" id="password"
                                         type="password" name="password" required autocomplete="new-password">
 
                                     @error('password')
@@ -61,18 +61,18 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label class="col-md-4 col-form-label text-md-end"
+                                <label class="mb-2 block text-lg font-bold text-gray-900 dark:text-white"
                                     for="password-confirm">{{ __('Confirm Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input class="form-control" id="password-confirm" type="password"
+                                    <input class="form-control block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" id="password-confirm" type="password"
                                         name="password_confirmation" required autocomplete="new-password">
                                 </div>
                             </div>
 
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button class="btn btn-primary" type="submit">
+                                    <button class="btn btn-primary mb-2 me-2 cursor-pointer rounded-lg border border-gray-800 px-5 py-2.5 text-center text-lg font-bold text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800" type="submit">
                                         {{ __('Register') }}
                                     </button>
                                 </div>
