feb062edb7a83fad883aec671cac86eb246c665d
abhishek-shrestha224 <meabhi224@gmail.com>
2024-03-29 23:07:07 +0545

    removed navbar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,18 +8,6 @@
 </head>
 
 <body class="bg-dark text-light">
-    <nav class="shadow-accent bg-dark fixed top-0 z-10 w-screen px-12 py-4 shadow-sm">
-        <ul class="flex gap-12 text-2xl font-bold">
-            <li>
-                <a class="hover:text-accent" href="/">Home</a>
-            </li>
-            <li>
-                <a class="hover:text-accent" href="/orders">Orders</a>
-            </li>
-        </ul>
-
-    </nav>
-
     @yield('content')
 </body>
 
