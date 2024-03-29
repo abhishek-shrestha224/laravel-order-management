ef93a015cdecdecc4c99d33e03e2884d533639d5
abhishek-shrestha224 <meabhi224@gmail.com>
2024-03-29 19:23:51 +0545

    Added navbar

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.layout')
 
 @section('content')
-    <section class="w-screen py-20 px-10">
-        <h1 class="text-6xl font-bold font-heading mb-4">Our Products</h1>
+    <section class="w-screen py-16 px-10 z-1">
+        <h1 class="text-6xl font-bold font-heading mb-4">All Orders</h1>
         <div>
-            @foreach ($products as $product)
-                <div class="flex text-3xl font-bold">
-                    <p class="font-heading">{{ $product['name'] }}</p>-<p class="font-heading">{{ $product['weight'] }}</p>
+            @foreach ($orders as $order)
+                <div class="flex text-3xl font-bold border border-light rounded-lg h-20">
+                    <p class="font-heading">{{ $order['name'] }}</p>-<p class="font-heading">{{ $order['weight'] }}</p>
                 </div>
             @endforeach
         </div>
