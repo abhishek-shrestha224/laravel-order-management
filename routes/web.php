50c4157e3dbec4a3c38992202f18036fab2bdb2b
abhishek-shrestha224 <meabhi224@gmail.com>
2024-04-01 15:11:49 +0545

    added route groups and named routes

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,32 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $isLoggedIn = Auth::check(); // Check if the user is logged in
-    return view('welcome', ['isLoggedIn' => $isLoggedIn]); // Pass the variable to the view
+    $isLoggedIn = Auth::check();
+    return view('welcome', ['isLoggedIn' => $isLoggedIn]);
 });
 
-Route::get('/orders', [OrderController::class, 'index'])->middleware('auth');
-Route::get('/orders/create', [OrderController::class, 'create']);
-Route::post('/orders', [OrderController::class, 'store']);
-Route::get('/orders/{slug}', [OrderController::class, 'show'])->middleware('auth');
-Route::delete('/orders/{slug}', [OrderController::class, 'destroy'])->middleware('auth');
+Route::group(['prefix' => 'orders'], function () {
+    Route::get('/', [OrderController::class, 'index'])
+        ->middleware('auth')
+        ->name('orders.index');
+
+    Route::get('/create', [OrderController::class, 'create'])->name('orders.create');
+
+    Route::post('/', [OrderController::class, 'store'])->name('orders.store');
+
+    Route::get('/{slug}', [OrderController::class, 'show'])
+        ->middleware('auth')
+        ->name('orders.show');
+
+    Route::delete('/{slug}', [OrderController::class, 'destroy'])
+        ->middleware('auth')
+        ->name('orders.destroy');
+});
 
 Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('contents/edit/home', [ContentController::class, 'edit'])->middleware('auth');
+Route::get('contents/edit/home', [ContentController::class, 'edit'])
+    ->middleware('auth')
+    ->name('cms.edit.home');
